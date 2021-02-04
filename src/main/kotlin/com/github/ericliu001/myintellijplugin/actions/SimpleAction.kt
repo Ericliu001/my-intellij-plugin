4e78cb35c9fd3d670053610e07f62c87f8181644
Eric Liu <eric.liu@uber.com>
2021-02-04 15:52:53 +1100

    file chooser working!

--- src/main/kotlin/com/github/ericliu001/myintellijplugin/actions/SimpleAction.kt
+++ src/main/kotlin/com/github/ericliu001/myintellijplugin/actions/SimpleAction.kt
@@ -2,12 +2,23 @@ package com.github.ericliu001.myintellijplugin.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileChooser.FileChooser
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.ui.Messages
+import com.intellij.util.Consumer
 
 class SimpleAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
-        val project = event.project
 
-        Messages.showMessageDialog(project, "Hello World!", "greeting!", Messages.getInformationIcon())
+        val fileChooserDescriptor = FileChooserDescriptor(true, true, false, false, false, false)
+
+        fileChooserDescriptor.title = "Pick a file or a directory:"
+        fileChooserDescriptor.description = "We do this for fun."
+
+        FileChooser.chooseFile(fileChooserDescriptor, event.project, null, Consumer {
+
+            Messages.showMessageDialog(event.project, "You have chosen: ${it.path}", "Congratulations!", Messages.getInformationIcon())
+        })
+
     }
 }
